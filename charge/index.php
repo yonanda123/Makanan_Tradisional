e9725241bcc0520b51692214a1c4473f87ce1c9c
Yonanda <yonandaharyono@gmail.com>
2022-11-26 21:45:02 +0700

    charge

--- charge/index.php
+++ charge/index.php
@@ -9,7 +9,7 @@ $api_url = $is_production ?
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 
-if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
+if(!strpos($_SERVER['REQUEST_URI'], '/charge') ) {
   http_response_code(404); 
   echo "wrong path, make sure it's `/charge`"; exit();
 }
